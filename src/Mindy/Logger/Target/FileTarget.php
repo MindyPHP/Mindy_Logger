ba9593efe7a5006b7937c072b848e64cfbc4dcaa
Falaleev Maxim <max@studio107.ru>
2014-01-05 23:44:35 +0400

    add docs folder

---
+++ src/Mindy/Logger/Target/FileTarget.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 
+ *
+ * All rights reserved.
+ * 
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 05/01/14.01.2014 21:23
+ */
+
+namespace Mindy\Logger\Target;
+
+
+class FileTarget 
+{
+
+}
+
+ 
