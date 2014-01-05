40e5de3a907ff6501de386f3aef5aa8132273fc7
Falaleev Maxim <max@studio107.ru>
2014-01-06 01:41:15 +0400

    Add psr3 tests

---
+++ Tests/Cases/Logger/Psr3Test.php
@@ -0,0 +1,30 @@
+<?php
+
+use Mindy\Logger\Logger;
+use Mindy\Logger\Target\DummyTarget;
+use Tests\TestCase;
+
+class Psr3Test extends TestCase
+{
+    public function providerLevels()
+    {
+        return [
+            ['info'],
+            ['notice'],
+            ['debug'],
+            ['error'],
+            ['warning'],
+        ];
+    }
+
+    /**
+     * @dataProvider providerLevels
+     */
+    public function testLog($level)
+    {
+        $logger = new Logger();
+        $logger->$level('app', 'test');
+        $this->assertEquals(1, count($logger->messages));
+        $this->assertEquals($level, $logger->messages[0]['level']);
+    }
+}
